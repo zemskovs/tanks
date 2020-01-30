fc4f274e6fbfbf441cf6d0c8383ea74cbd7d4d97
zemskov <vladimir.zemskov@ics-it.ru>
2020-01-30 20:03:14 +0300

    inprogress

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import * as httpServer from "http";
 import * as sockets from "socket.io";
 import api from "./api/api";
 import { map } from "./map/map";
+import { tankController } from "./tankController/tankController";
 
 const app = express();
 const http = httpServer.createServer(app);
@@ -18,14 +19,21 @@ app.get(api.map, (req, res) => {
 	res.send(JSON.stringify(map));
 });
 
-io.on("connect", function(socket) {
+io.on("connection", function(socket) {
 	console.log("connect");
-	io.on("move", (socket) => {
-		io.emit("up")
-		console.log(socket)
-
+	socket.on("move1", (message) => {
+		const msg = JSON.parse(message);
+		console.log(msg)
+		switch (msg.action) {
+			case "up": {				
+				io.emit("up1", JSON.stringify(tankController.up(msg.player)))
+				break
+			}
+		}
 	})
+
 });
+
 
 http.listen(3000, function() {
 	console.log("listening on *:3000");
--- server/tankController/tankController.ts
+++ server/tankController/tankController.ts
@@ -1,85 +1,85 @@
 import { map } from "../map/map";
 import {iYCnt} from "./consts"
 
-class TankControoler {
+class TankController {
 	map = map;
-	player;
+	// player;
 
-	constructor(player) {
-		this.player = player;
-	}
-
-	up() {
-		this.player.i = 2;
+	up(player) {
+		player.i = 2;
 
 		// checking collisions
-		var iCurCelX = (2 * this.player.x) / 48;
-		var iCurCelY = (2 * this.player.y) / 48;
+		var iCurCelX = (2 * player.x) / 48;
+		var iCurCelY = (2 * player.y) / 48;
 		if (iCurCelY) {
 			var iTest1 = this.map[iCurCelY - 1][iCurCelX];
 			var iTest2 = this.map[iCurCelY - 1][iCurCelX + 1];
 
 			if ((iTest1 == 0 || iTest1 == 3) && (iTest2 == 0 || iTest2 == 3)) {
-				this.player.y -= 24;
-				if (this.player.y < 0) {
-					this.player.y = 0;
+				player.y -= 24;
+				if (player.y < 0) {
+					player.y = 0;
 				}
 			}
 		}
+		console.log(player)
+		return player
 	}
 
-	down() {
-		this.player.i = 3;
+	// down() {
+	// 	this.player.i = 3;
 
-		// checking collisions
-		var iCurCelX = (2 * this.player.x) / 48;
-		var iCurCelY = (2 * this.player.y) / 48;
-		if (iCurCelY + 2 < iYCnt) {
-			var iTest1 = this.map[iCurCelY + 2][iCurCelX];
-			var iTest2 = this.map[iCurCelY + 2][iCurCelX + 1];
+	// 	// checking collisions
+	// 	var iCurCelX = (2 * this.player.x) / 48;
+	// 	var iCurCelY = (2 * this.player.y) / 48;
+	// 	if (iCurCelY + 2 < iYCnt) {
+	// 		var iTest1 = this.map[iCurCelY + 2][iCurCelX];
+	// 		var iTest2 = this.map[iCurCelY + 2][iCurCelX + 1];
 
-			if ((iTest1 == 0 || iTest1 == 3) && (iTest2 == 0 || iTest2 == 3)) {
-				this.player.y += 24;
-				if (this.player.y > 576) {
-					//iCellSize * (iYCnt-2)
-					this.player.y = 576;
-				}
-			}
-		}
-	}
+	// 		if ((iTest1 == 0 || iTest1 == 3) && (iTest2 == 0 || iTest2 == 3)) {
+	// 			this.player.y += 24;
+	// 			if (this.player.y > 576) {
+	// 				//iCellSize * (iYCnt-2)
+	// 				this.player.y = 576;
+	// 			}
+	// 		}
+	// 	}
+	// }
 
-	right() {
-		this.player.i = 0;
+	// right() {
+	// 	this.player.i = 0;
 
-		// checking collisions
-		var iCurCelX = (2 * this.player.x) / 48;
-		var iCurCelY = (2 * this.player.y) / 48;
-		var iTest1 = this.map[iCurCelY][iCurCelX + 2];
-		var iTest2 = this.map[iCurCelY + 1][iCurCelX + 2];
+	// 	// checking collisions
+	// 	var iCurCelX = (2 * this.player.x) / 48;
+	// 	var iCurCelY = (2 * this.player.y) / 48;
+	// 	var iTest1 = this.map[iCurCelY][iCurCelX + 2];
+	// 	var iTest2 = this.map[iCurCelY + 1][iCurCelX + 2];
 
-		if ((iTest1 == 0 || iTest1 == 3) && (iTest2 == 0 || iTest2 == 3)) {
-			this.player.x += 24;
-			if (this.player.x > 576) {
-				//iCellSize * (iXCnt-2)
-				this.player.x = 576;
-			}
-		}
-	}
+	// 	if ((iTest1 == 0 || iTest1 == 3) && (iTest2 == 0 || iTest2 == 3)) {
+	// 		this.player.x += 24;
+	// 		if (this.player.x > 576) {
+	// 			//iCellSize * (iXCnt-2)
+	// 			this.player.x = 576;
+	// 		}
+	// 	}
+	// }
 
-	left() {
-		this.player.i = 1;
+	// left() {
+	// 	this.player.i = 1;
 
-		// checking collisions
-		var iCurCelX = (2 * this.player.x) / 48;
-		var iCurCelY = (2 * this.player.y) / 48;
-		var iTest1 = this.map[iCurCelY][iCurCelX - 1];
-		var iTest2 = this.map[iCurCelY + 1][iCurCelX - 1];
+	// 	// checking collisions
+	// 	var iCurCelX = (2 * this.player.x) / 48;
+	// 	var iCurCelY = (2 * this.player.y) / 48;
+	// 	var iTest1 = this.map[iCurCelY][iCurCelX - 1];
+	// 	var iTest2 = this.map[iCurCelY + 1][iCurCelX - 1];
 
-		if ((iTest1 == 0 || iTest1 == 3) && (iTest2 == 0 || iTest2 == 3)) {
-			this.player.x -= 24;
-			if (this.player.x < 0) {
-				this.player.x = 0;
-			}
-		}
-	}
+	// 	if ((iTest1 == 0 || iTest1 == 3) && (iTest2 == 0 || iTest2 == 3)) {
+	// 		this.player.x -= 24;
+	// 		if (this.player.x < 0) {
+	// 			this.player.x = 0;
+	// 		}
+	// 	}
+	// }
 }
+
+export const tankController = new TankController()
